6b518e55098d9f41eefbe496779b7d738a702fcf
TRUMANCFY <caifengyutruman@gmail.com>
2025-04-29 11:30:17 +0000

    formulate the config

---
+++ scripts/contriever/msmarco_prop_frozen_logsumexp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+export ROOT_DIR=/pfss/mlde/workspaces/mlde_wsp_PI_Heinz/cai/dpr-scale
+export DATA_DIR=$ROOT_DIR/data
+export OUTPUT_DIR=$ROOT_DIR/logs
+export MODEL_DIR=$ROOT_DIR/models
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+export PYTHONPATH="$ROOT_DIR":$PYTHONPATH
+export HYDRA_FULL_ERROR=1
+# export WANDB_API_KEY="6524c9fb101a70ff6712c8d66740fcfd289a2486"
+# export WANDB_PROJECT="mixgr"
+# export WANDB_ENTITY="my-team" 
+# export WANDB_MODE=dryrun
+
+python $ROOT_DIR/dpr_scale/main.py --config-name msmarco_baseline_contriever_prop_frozen_logsumexp.yaml \
+    logger.name=contriever.prop.frozen.logsumexp \
+    checkpoint_callback.dirpath="$MODEL_DIR"/contriever.prop.frozen.logsumexp/checkpoints
---
+++ scripts/dpr/msmarco_prop_trainable_logsumexp.ctx.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export ROOT_DIR=/pfss/mlde/workspaces/mlde_wsp_PI_Heinz/cai/dpr-scale
+export DATA_DIR=$ROOT_DIR/data
+export OUTPUT_DIR=$ROOT_DIR/logs
+export MODEL_DIR=$ROOT_DIR/models
+
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+
+export PYTHONPATH="$ROOT_DIR":$PYTHONPATH
+export HYDRA_FULL_ERROR=1
+
+python $ROOT_DIR/dpr_scale/main.py --config-name msmarco_baseline_dpr_prop_trainable_logsumexp_ctx.yaml \
+    logger.name=dpr.prop.trainable.logsumexp.ctx \
+    checkpoint_callback.dirpath="$MODEL_DIR"/dpr.prop.trainable.logsumexp.ctx/checkpoints
+
