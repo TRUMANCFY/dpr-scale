c85bdbdb0992bb9bbbaeb2a8ab9022916ba1c031
TRUMANCFY <caifengyutruman@gmail.com>
2025-05-05 09:15:01 +0000

    upload conf

---
+++ scripts/contriever/msmarco_bert_prop_trainable_maxprop_8.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+export ROOT_DIR=/pfss/mlde/workspaces/mlde_wsp_PI_Heinz/cai/dpr-scale
+export DATA_DIR=$ROOT_DIR/data
+export OUTPUT_DIR=$ROOT_DIR/logs
+export MODEL_DIR=$ROOT_DIR/models
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+export PYTHONPATH="$ROOT_DIR":$PYTHONPATH
+export HYDRA_FULL_ERROR=1
+
+python $ROOT_DIR/dpr_scale/main.py --config-name msmarco_baseline_bert_contriever_prop_trainable_maxprop_8.yaml \
+    logger.name=bert.contriever.prop.trainable.maxprop.8 \
+    checkpoint_callback.dirpath="$MODEL_DIR"/bert.contriever.prop.trainable.maxprop.8/checkpoints
